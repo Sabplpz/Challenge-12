5fdece2c82e91874be522ab46aef15587830b955
Sabplpz <sabrinalpz1994@gmail.com>
2023-05-14 12:51:55 -0400

    Adding new functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO department (id, name)
-VALUES (001, "Make-up");
+INSERT INTO department (name)
+VALUES ("Make-up");
        
-INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "sales associate", 14.5, 001),
-       (002, "manager", 20.5, 001);
+INSERT INTO role (title, salary, department_id)
+VALUES ("sales associate", 14.5, 1),
+       ("manager", 20.5, 1);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (001, "Ana", "Auad", 001, 005),  
-       (005, "Ana", "Stiles", 002, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ana", "Auad", 1, 2),  
+       ("Ana", "Stiles", 2, NULL);
--- server.js
+++ server.js
@@ -12,47 +12,79 @@ const db = mysql.createConnection(
     console.log(`Connected to the business_db database.`)
   );
 
-  
+function viewEmployee() {
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+    mainMenu();
+  });
+}
 
+function viewDepartment() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+    mainMenu();
+  });
+}
+
+function viewRole() {
+  db.query('SELECT * FROM role', function (err, results) {
+    console.log(results);
+    mainMenu();
+  });
+}
+
+function addDepartment() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What's the name of the new department?"
+    }
+  ])
+  .then((answers) => {
+    db.query(`INSERT INTO department (name) VALUES ("${answers.name}")`, function (err, results) {
+      console.log("New department has been added!");
+      mainMenu();
+    });
+  });
+}
+
+function mainMenu() {
   inquirer
   .prompt([
     {
       type: 'list',
       name: 'view',
       message: 'What do you want to view?',
-      choices: ["Employee", "Role", "Department"]
+      choices: ["Employee", "Role", "Department", "Add Department"],
     },
   ])
   .then(answers => {
 
     switch (answers.view) {
       case 'Employee':
-        db.query('SELECT * FROM employee', function (err, results) {
-          console.log(results);
-        });
+        viewEmployee();
         break;
       case 'Role':
-        db.query('SELECT * FROM role', function (err, results) {
-          console.log(results);
-        });
+        viewRole();
         break;
       case 'Department':
-        db.query('SELECT * FROM department', function (err, results) {
-          console.log(results);
-        });
+        viewDepartment();
+        break;
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'Add Employee':
+        addEmployee();
         break;
       default:
         console.log('Oh oh! Choose again!');
     }
-  })
-  .prompt([
-    {
-      type: 'list',
-      name: 'view',
-      message: 'What do you want to view?',
-      choices: ["Employee", "Role", "Department"]
-    },
-  ])
-  
+  });
+};
 
-
+mainMenu();
